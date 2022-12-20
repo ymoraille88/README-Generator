ed43ac8f59775082770b138689f0687d3dbfa71f
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-20 18:03:01 -0500

    added the README questions

--- utils/index.js
+++ utils/index.js
@@ -2,22 +2,47 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: "What is your name?",
-      name: 'name',
-    },
-    {
-      type: 'list', 
-      message: "What languages do you know?",
-      name: 'languages',
-      
-    },
-    {
-      type: 'rawlist',
-      message: "What is your preferred method of communication?",
-      name: 'communications',
-      
-    },
-  ])
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the title of your project?",
+            name: 'title',
+            validate: input => {
+                if (input) {
+                    return true;
+                } else {
+                    console.log('please enter the title of your project.')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            message: "A description of your project",
+            name: 'description',
+            validate: input => {
+                if (input) {
+                    return true;
+                } else {
+                    console.log('please provide a description if you do')
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            message: "what are the steps to install the project?",
+            name: 'installation',
+            validate: input => {
+                if (input) {
+                    return true;
+                } else {
+                    console.log('how do we install your project?')
+                    return false;
+
+                }
+            }
+        },
+    ])
